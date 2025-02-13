4265eccba33eeea7756ffecca08f8b357a6c7a92
MOSALAHDEV <mosalah525446@gmail.com>
2025-02-12 17:50:01 -0800

    Add

--- models/state.py
+++ models/state.py
@@ -7,8 +7,8 @@ class State(BaseModel):
     """ State class """
     name = ""
     cities = []
-    def __init__(self, *args, **kwargs):
-        """Initializes a new State instance"""
-        super().__init__(*args, **kwargs)
-        if 'cities' in kwargs:
-            self.cities = kwargs['cities']
+   # def __init__(self, *args, **kwargs):
+    #    """Initializes a new State instance"""
+     #   super().__init__(*args, **kwargs)
+      #  if 'cities' in kwargs:
+       #     self.cities = kwargs['cities']
