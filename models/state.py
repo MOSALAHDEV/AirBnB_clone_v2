c12c36640347cf1ad6ba31425e5fc12fb1943b91
MOSALAHDEV <mosalah525446@gmail.com>
2025-02-12 17:37:25 -0800

    Fix: fixed state.py to add new instances

--- models/state.py
+++ models/state.py
@@ -6,3 +6,9 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+    cities = []
+    def __init__(self, *args, **kwargs):
+        """Initializes a new State instance"""
+        super().__init__(*args, **kwargs)
+        if 'cities' in kwargs:
+            self.cities = kwargs['cities']
