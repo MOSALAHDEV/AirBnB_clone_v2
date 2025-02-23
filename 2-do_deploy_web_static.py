95744712c2fe214945a777bbedf1d01ea3d447f0
MOSALAHDEV <mosalah525446@gmail.com>
2025-02-23 15:53:25 -0800

    Add

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Fabric script that distributes an archive to web servers """
-from fabric.api import env, local, run, put
+from fabric.api import *
 import os
 from datetime import datetime
 
