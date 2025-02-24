4d1aed5ac9fee7f6259cda15a0c2f46852a8af0c
MOSALAHDEV <mosalah525446@gmail.com>
2025-02-23 17:38:09 -0800

    add

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,40 +5,30 @@ import os
 from datetime import datetime
 
 
+env.hosts = ['100.26.168.254', '44.211.253.220']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
-env.hosts = ['100.26.168.254', '44.211.253.220']
 
 
 def do_pack():
     """ Generates a .tgz archive from the contents of web_static """
     try:
-        local('mkdir -p versions')
-        date = datetime.now()
-        filename = f'versions/web_static_{date.strftime("%Y%m%d%H%M%S")}.tgz'
-        local(f'tar -cvzf {filename} web_static')
-        return filename
-    except:
+        time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = f"versions/web_static_{time_stamp}.tgz"
+        local("mkdir -p versions")
+        local(f"tar -cvzf {archive_path} web_static")
+        return archive_path
+    except Exception as e:
         return None
-
-def deploy():
-    """ Distributes an archive to web servers """
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    return do_deploy(archive_path)
 
 def do_deploy(archive_path):
     """ Distributes an archive to web servers """
     if not os.path.exists(archive_path):
         return False
     try:
-        form = '%Y-%m-%dT%H:%M:%S.%f'
-        filename = os.path.basename(archive_path)
-        without_ex = filename.split('.')[0]
-        date = datetime.strptime(without_ex, form)
-        release_path = f'/data/web_static/releases/web_static_{date.strftime("%Y%m%d%H%M%S")}'
         put(archive_path, '/tmp/')
+        filename = os.path.basename(archive_path)
+        release_path = f'/data/web_static/releases/web_static_{filename.split(".")[0]}'
         run(f'mkdir -p {release_path}')
         run(f'tar -xzf /tmp/{filename} -C {release_path}')
         run(f'rm /tmp/{filename}')
@@ -47,5 +37,12 @@ def do_deploy(archive_path):
         run(f'rm -rf /data/web_static/current')
         run(f'ln -s {release_path}/ /data/web_static/current')
         return True
-    except Exception as e:
+    except:
         return False
+
+def deploy():
+    """ Distributes an archive to web servers """
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
