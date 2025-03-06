54888cf3b83ebc6f4831c970d1511cd9ed3a9f7f
MOSALAHDEV <mosalah525446@gmail.com>
2025-03-05 23:13:52 -0800

    add

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This script starts a Flask web application. """
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -39,7 +40,7 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
+def number_template(n=None):
     """ This function returns a string """
     if isinstance(n, int):
         return render_template('5-number.html', n=n)
