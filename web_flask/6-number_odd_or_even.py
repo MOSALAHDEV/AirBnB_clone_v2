82d5d409384cd1fe4bdd57f81acb36063a914e21
MOSALAHDEV <mosalah525446@gmail.com>
2025-03-06 00:21:37 -0800

    add

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,7 +44,7 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    """ This function renders an HTML template with 'odd' or 'even' based on n """
+    """ This function renders an HTML template with 'odd' or 'even'"""
     return render_template('6-number_odd_or_even.html', n=n, even=(n % 2 == 0))
 
 
